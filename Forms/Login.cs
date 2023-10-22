46f05bfb6737feef4dc48d6af7f652c87b1c73d4
Homer Martillano <hamartillano@gmail.com>
2023-10-22 22:33:04 +1100

    simple login page created

---
+++ Forms/Login.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchoolManagementSystem.Forms
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button_login_Click(object sender, EventArgs e)
+        {
+            string username = "admin";
+            string password = "password";
+
+            string inputUsername = textBox_username.Text;
+            string inputPassword = textBox_password.Text;
+
+            if (inputUsername == username && inputPassword == password)
+            {
+                MessageBox.Show("Login successful!");
+            }
+            else
+            {
+                MessageBox.Show("Login failed. Please check your credentials.");
+            }
+        }
+    }
+}
