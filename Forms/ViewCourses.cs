ee604abfa659bb20f351a337d0d3aca0e1a119a7
Homer Martillano <hamartillano@gmail.com>
2023-10-27 15:32:57 +1100

    delete course function developed

--- Forms/ViewCourses.cs
+++ Forms/ViewCourses.cs
@@ -92,7 +92,55 @@ namespace SchoolManagementSystem.Forms
 
         private void button_deleteCourse_Click(object sender, EventArgs e)
         {
+            if (listView1.SelectedItems.Count == 1)
+            {
+                ListViewItem selectedItem = listView1.SelectedItems[0];
 
+                int selectedCourseID;
+                if (int.TryParse(selectedItem.SubItems[0].Text, out selectedCourseID))
+                {
+                    Course selectedCourse = CurrentTeacher.TeachingCourses.FirstOrDefault(course => course.CourseID == selectedCourseID);
+
+                    if (selectedCourse != null)
+                    {
+                        DialogResult result = MessageBox.Show($"Are you sure you want to delete the course: {selectedCourse.CourseName}?", "Confirm Deletion", MessageBoxButtons.YesNo);
+                        if (result == DialogResult.Yes)
+                        {
+                            List<string> lines = File.ReadAllLines("courses.txt").ToList();
+
+                            bool courseFound = false;
+                            for (int i = 0; i < lines.Count; i++)
+                            {
+                                string[] parts = lines[i].Split(',');
+                                if (parts.Length == 3)
+                                {
+                                    int courseID = int.Parse(parts[0]);
+                                    if (courseID == selectedCourse.CourseID)
+                                    {
+                                        lines.RemoveAt(i);
+                                        courseFound = true;
+                                        break;
+                                    }
+                                }
+                            }
+
+                            if (courseFound)
+                            {
+                                File.WriteAllLines("courses.txt", lines);
+                                LoadTeachingCourses();
+                            }
+                            else
+                            {
+                                MessageBox.Show("Selected course not found in the courses.txt file.");
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a course to delete.");
+            }
         }
 
         private void button_createCourse_Click(object sender, EventArgs e)
