8b5d6a28e370be89ecd6d03c2956fd6af8282a5d
Homer Martillano <hamartillano@gmail.com>
2023-10-30 09:37:35 +1100

    ui fix

--- Forms/EnrollandUnenrollCourse.Designer.cs
+++ Forms/EnrollandUnenrollCourse.Designer.cs
@@ -48,7 +48,7 @@
             this.listView1.Location = new System.Drawing.Point(16, 20);
             this.listView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(385, 213);
+            this.listView1.Size = new System.Drawing.Size(438, 213);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -75,7 +75,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(16, 235);
+            this.button1.Location = new System.Drawing.Point(16, 240);
             this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(175, 28);
@@ -86,7 +86,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(205, 234);
+            this.button2.Location = new System.Drawing.Point(259, 240);
             this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(195, 29);
@@ -99,7 +99,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(415, 279);
+            this.ClientSize = new System.Drawing.Size(477, 279);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listView1);
